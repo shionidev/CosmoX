9ff1c73560b0d3eac60cfc4726bd044507f4753d
AquaVirtual <aquavirtual.aqua@gmail.com>
2023-12-16 04:50:22 +0700

    System Reworked & Changed audio default quality from 128 kbps to 256 kbps

---
+++ CosmoX.Game/Rework/Assets/Scripts/Game/Buttons/SceneTransfer.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a4fa2b3cd9d90caf086ac1dd966d7add90dff8eb50cb35e65ff2696914fda9ba
+size 650
---
+++ CosmoX.Game/Rework/Assets/Scripts/System/Product/GetVersion.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:77d440d99890ff195e4512389a968cf2587732a06f9874d166eb6af08a149e79
+size 301
