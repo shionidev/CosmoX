cc82d6f47f3dab8e3c933e8c1b70f0a240f6f0fa
AquaVirtual <aquavirtual.aqua@gmail.com>
2023-12-17 09:00:04 +0700

    Removed Tooltip

--- CosmoX.Game/Rework/Assets/Scripts/Game/Features/Tooltip/Tooltip.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d4db88dc433adf32343b9e531d2a4f901adb2b315c7729382f662fa90553b001
-size 360
--- CosmoX.Game/Rework/Assets/Scripts/Game/Features/Tooltip/TooltipManager.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:98d599187ab262e1450fea213ace95cc5d7357749e0909e22e21296da8c0c5e7
-size 1060
