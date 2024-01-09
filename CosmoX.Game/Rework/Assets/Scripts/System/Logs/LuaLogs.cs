d56579f53799693a42c6a89a1380b19e47af43aa
AquaVirtual <aquavirtual.aqua@gmail.com>
2024-01-09 19:24:19 +0700

    Added Logs for Lua

---
+++ CosmoX.Game/Rework/Assets/Scripts/System/Logs/LuaLogs.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Configuration;
+
+public class LuaLogs : MonoBehaviour
+{
+    // Classic Information System Logs
+    public void IsRunningLua()
+    {
+        Debug.Log("Lua is running...");
+    }
+}
